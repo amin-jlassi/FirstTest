c5df433098118a99fbeb40d251ecd78ea63495f1
amin <pcjlassi@gmail.com>
2024-12-04 21:45:34 +0100

    p file

---
+++ objects.py
@@ -0,0 +1,7 @@
+class User():
+    def __init__(self , name , email , password):
+        self.name = name 
+        self.email = email  
+        self.password = password
+
+user1 = User('Gogo' , 'amin@gmail.com' , 'aeaze55')
